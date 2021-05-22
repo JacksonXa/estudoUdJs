dbf83cbb25cdfd8683fed9831253c2316f5d56b6
Jackson <ja.xavier06@gmail.com>
2021-05-22 19:42:43 -0300

    Estudo Udemy JavaScript

---
+++ SECAO8_ARRAY_OBJETOS/11_push_e_pop/scripts.js
@@ -0,0 +1,10 @@
+let nomes = ["Jackson", "Amanda", "Xavier"];
+
+let elementoRemovido = nomes.pop();
+
+console.log(elementoRemovido);
+console.log(nomes);
+
+nomes.push("Rhukia");
+
+console.log(nomes);
