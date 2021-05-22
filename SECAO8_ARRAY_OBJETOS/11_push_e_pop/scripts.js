5871881d3856e980e51c54827bb7b3cb37cf2e18
Jackson <ja.xavier06@gmail.com>
2021-05-22 20:00:41 -0300

    Estudo Udemy JavaScript

--- SECAO8_ARRAY_OBJETOS/11_push_e_pop/scripts.js
+++ SECAO8_ARRAY_OBJETOS/11_push_e_pop/scripts.js
@@ -1,10 +1,14 @@
+//Adicionar e remover nome do array de nomes
+
 let nomes = ["Jackson", "Amanda", "Xavier"];
 
+//Pop remove o elemento do array
 let elementoRemovido = nomes.pop();
 
 console.log(elementoRemovido);
 console.log(nomes);
 
+//Push adiciona o elemento ao array
 nomes.push("Rhukia");
 
 console.log(nomes);
---
+++ SECAO8_ARRAY_OBJETOS/12_shift_unshift/scripts.js
@@ -0,0 +1,23 @@
+/**
+ * Criar um array de marcas de motos
+ * Remover a primeira marca do array e exibir no console a marca removida
+ * Exibir como ficou o array no console
+ * Adicionar uma marca de moto ao array e exibir no console a marca adicionada
+ * Exibir como ficou o array de motos
+ */
+
+let motos = ["Honda", "BMW", "Yamaha", "Suzuki"];
+
+//Remove a primeira moto do Array
+let removerPrimeiraMoto = motos.shift();
+
+//Retorna a moto removida
+console.log(removerPrimeiraMoto);
+//Retona o array de motos
+console.log(motos);
+
+//Adiciona um marca de motos ao inicio do array e retorna no coonsole
+motos.unshift('Dafra');
+console.log(motos)
+
+
