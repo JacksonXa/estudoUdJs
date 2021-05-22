d6dec5be8c9cade984e117afcc4c2c1f2c4262bb
Jackson <ja.xavier06@gmail.com>
2021-05-22 20:14:43 -0300

    Estudo Udemy JavaScript

---
+++ SECAO8_ARRAY_OBJETOS/13_indexOf_lastIndexOf/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * indexOf = Encontra qual o indice da primeira ocorrencia de um elemento no array
+ * lastIndexOf = Encontra qual o indice da ultima ocorrencia de um elemento no array 
+ */
+
+let letras = ["A","B","C","B","C","A","C"];
+
+//Irá retornar 0, pois a primeira ocorrencia de A esta na posição 0 do array
+console.log(letras.indexOf("A"));
+
+//Irpa retornar 5, pois a ultima ocorrencia de A esta na posição 5 do array
+console.log(letras.lastIndexOf("A"));
